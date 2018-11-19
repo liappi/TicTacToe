bd3c9ea7131af27c59e806e2bc3fca9ac4bab547
liappi <li.vilia039@gmail.com>
2018-11-19 16:14:43 +1300

    deleted unused functions and variables

--- TicTacToe/Player.cs
+++ TicTacToe/Player.cs
@@ -2,9 +2,6 @@ using System;
 
 namespace TicTacToe {
     public class Player {
-        
-        public int row;
-        public int col;
         public bool inputValid;
         public bool givenUp;
         public char symbol;
@@ -14,12 +11,6 @@ namespace TicTacToe {
             inputValid = false;
             givenUp = false;
             this.symbol = symbol;
-        }
-
-        private void updateRowAndCol(string input) {
-            var coordinates = input.Split(',');
-            col = Convert.ToInt32(coordinates[0]);
-            row = Convert.ToInt32(coordinates[1]);
         }
     }
 }
