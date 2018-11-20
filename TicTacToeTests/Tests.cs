48cb5c887d4ecc44a34a267b4d55e775965f7443
liappi <li.vilia039@gmail.com>
2018-11-20 15:02:48 +1300

    example unit testing from karen

--- TicTacToeTests/Tests.cs
+++ TicTacToeTests/Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using NUnit.Framework;
 using TicTacToe;
 
@@ -11,6 +12,43 @@ namespace TicTacToeTests {
             var inputValidator = new InputValidator(gameBoard);
             
             Assert.False(inputValidator.playerInputIsValid("11"));
+        }
+
+        [Test]
+        public void ShouldReturnEmpty() {
+            var game = new TicTacToe();
+            
+            var result = game.Play();
+
+            Assert.AreEqual("..." +
+                            "..." +
+                            "...", result);
+        }
+
+        [Test]
+        public void GivenPositionAndTokenShouldPlaceTokenAtThatPositionOnBoard() {
+            var game = new TicTacToe();
+
+            var result = game.MakeMove(1, 'X', "..." +
+                                               "..." +
+                                               "...");
+            
+            Assert.AreEqual(".X." +
+                            "..." +
+                            "...", result);
+        }
+    }
+
+    public class TicTacToe {
+        public string Play() {
+            return "..." +
+                   "..." +
+                   "...";
+        }
+
+        public string MakeMove(int position, char token, string board) {
+            var outputBoard = new StringBuilder(board) {[position] = token};
+            return outputBoard.ToString();
         }
     }
 }
